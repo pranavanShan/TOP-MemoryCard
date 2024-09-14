57ffcd8532bc8f8274214b3c48027d6528ecb7c9
PranavanShan <pranavanshan@gmail.com>
2024-09-14 11:23:43 +0100

    Project setup + skeleton code. actually using TDD and linter this time

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+// import './index.css' for syling removed all default files to readd with ebtter names n stuff
+// use this for testing (should prob use TDD a little more also actually use linter)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+  </StrictMode>,
+)
