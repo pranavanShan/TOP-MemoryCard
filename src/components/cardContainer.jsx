3595ed671b17d9e16fee3a1fd4c7166a111f4561
PranavanShan <pranavanshan@gmail.com>
2024-09-17 13:14:43 +0100

    Made functional memory game but have not fully used components

--- src/components/cardContainer.jsx
+++ src/components/cardContainer.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-
+import { Scores } from "./scores";
 // not mine: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
 function shuffle(array) {
     let currentIndex = array.length;
@@ -9,19 +9,22 @@ function shuffle(array) {
       [array[currentIndex], array[randomIndex]] = [
         array[randomIndex], array[currentIndex]];
     }
-
     return array
+  }
+
+
+function alreadyClicked(clicked, setClicked, url) {
+    if(clicked.includes(url)) return true;
+    else setClicked([...clicked, url])
+    return false
   }
 
 function CardContainer() {
   const [gifs, setGifs] = useState(false);
-
+  const [clicked, setClicked] = useState([])
+  const [score, setScore] = useState(0)
+  const [best, setBest] = useState(0)
   useEffect(() => {
-    // i think this is optimal but idk
-    //first use this funciton which makes a list of gifs
-    //because we are relying on an api this is wrapped around a useeffect
-    //then we use the results to make cards wtih card componenet
-    //beagain remember that you have to be able to random sort these
     fetch(
       "https://api.giphy.com/v1/gifs/search?q=cat&limit=10&api_key=xAXwA9hrraLD33fYZTvcYMlTLjkyNUVX",
       { mode: "cors" }
@@ -35,8 +38,7 @@ function CardContainer() {
     <>
       {gifs && (
         <div>
-            {/* need to somehow make it so you cant have 3 that have all been clicked on before */}
-          {gifs.slice(0,3).map((item) => {
+          {gifs.map((item) => {
             return (
               <img
                 key={item.id}
@@ -44,11 +46,22 @@ function CardContainer() {
                 src={item.images.original.url}
                 onClick={() => {
                   setGifs([...shuffle(gifs)]);
+                  if(alreadyClicked(clicked, setClicked, item.images.original.url)) {
+                    setClicked([])
+                    setScore(0)
+                    if(score > best) setBest(score)
+                  } else {
+                    setScore(score + 1)
+                  }
                 }}
                 alt="gif"
               />
             );
           })}
+          <Scores
+          score={score}
+          bestScore={best}
+          ></Scores>
         </div>
       )}
     </>
---
+++ src/components/scores.jsx
@@ -0,0 +1,13 @@
+
+/* eslint-disable react/prop-types */
+function Scores({score, bestScore}) {
+    return (
+        <>
+            <p> Score: {score} </p>
+            <p> Best Score: {bestScore}</p>
+        </>
+    )
+}
+
+
+export {Scores }
